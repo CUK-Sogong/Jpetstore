4debaad88d92cddffc10fd5cafcc448eb9a07b78
ktykty0722 <ktykty0722@naver.com>
2021-11-22 20:48:26 +0900

    UserSalesActionBean 일부 추가

---
+++ src/main/java/org/mybatis/jpetstore/web/actions/UserSalesActionBean.java
@@ -0,0 +1,41 @@
+package org.mybatis.jpetstore.web.actions;
+
+import java.util.List;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.SessionScope;
+import net.sourceforge.stripes.integration.spring.SpringBean;
+import javax.servlet.http.HttpSession;
+
+/**
+ * The Class UserSalesActionBean.
+ *
+ * @author Eduardo Macarron
+ */
+@SessionScope
+public class UserSalesActionBean extends AbstractActionBean {
+
+    private static final long serialVersionUID = 5849523372175050635L;
+
+    private static final String SALES_LIST = "/WEB-INF/jsp/user_sales/UserSalesList.jsp";
+    private static final String SELECT_SALES = "/WEB-INF/jsp/user_sales/SelectSalesForm.jsp";
+    private static final String INSERT_SALES = "/WEB-INF/jsp/user_sales/InsertSalesForm.jsp";
+    private static final String INFO_SALES = "/WEB-INF/jsp/user_sales/InfoSalesForm.jsp";
+    private static final String INSERT_ADOPT = "/WEB-INF/jsp/user_sales/InsertAdoptForm.jsp";
+    private static final String VIEW_ADOPT_LIST_ADT = "/WEB-INF/jsp/user_sales/ViewAdoptListForAdopter.jsp";
+    private static final String VIEW_ADOPT_ADT = "/WEB-INF/jsp/user_sales/ViewAdoptForAdopter.jsp";
+    private static final String VIEW_ADOPT_LIST_SL = "/WEB-INF/jsp/user_sales/ViewAdoptListForSales.jsp";
+    private static final String VIEW_ADOPT_SL = "/WEB-INF/jsp/user_sales/ViewAdoptForSales.jsp";
+
+    /**
+     * View Sales List.
+     *
+     * @return the resolution
+     */
+    @DefaultHandler
+    public Resolution viewSalesList() {
+        return new ForwardResolution(SALES_LIST);
+    }
+
+}
