23267f30988c03809a2e1ae4afb7fcf157fce900
ktykty0722 <ktykty0722@naver.com>
2021-11-24 12:22:18 +0900

    import , 변수 추가

--- src/main/java/org/mybatis/jpetstore/web/actions/UserSalesActionBean.java
+++ src/main/java/org/mybatis/jpetstore/web/actions/UserSalesActionBean.java
@@ -5,6 +5,11 @@ import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.SessionScope;
+import org.mybatis.jpetstore.domain.UserSale;
+import org.mybatis.jpetstore.domain.UserAdopt;
+import org.mybatis.jpetstore.domain.Account;
+import org.mybatis.jpetstore.service.UserSalesService;
+import org.mybatis.jpetstore.service.AccountService;
 import net.sourceforge.stripes.integration.spring.SpringBean;
 import javax.servlet.http.HttpSession;
 
@@ -24,10 +29,30 @@ public class UserSalesActionBean extends AbstractActionBean {
     private static final String EDIT_SALES = "/WEB-INF/jsp/user_sales/EditSalesForm.jsp";
     private static final String INFO_SALES = "/WEB-INF/jsp/user_sales/InfoSales.jsp";
     private static final String INSERT_ADOPT = "/WEB-INF/jsp/user_sales/InsertAdoptForm.jsp";
+    private static final String UPDATE_ADOPT = "/WEB-INF/jsp/user_sales/UpdateAdoptForm.jsp";
     private static final String VIEW_ADOPT_LIST_ADT = "/WEB-INF/jsp/user_sales/ViewAdoptListForAdopter.jsp";
     private static final String VIEW_ADOPT_ADT = "/WEB-INF/jsp/user_sales/ViewAdoptForAdopter.jsp";
     private static final String VIEW_ADOPT_LIST_SL = "/WEB-INF/jsp/user_sales/ViewAdoptListForSales.jsp";
     private static final String VIEW_ADOPT_SL = "/WEB-INF/jsp/user_sales/ViewAdoptForSales.jsp";
+
+
+    @SpringBean
+    private transient UserSalesService userSalesService;
+
+    private List<UserSale> userSaleList;
+    private List<UserAdopt> userAdoptsList;
+    private UserSale userSale;
+    private UserAdopt userAdopt;
+
+    private Account account = new Account();
+
+    public List<UserSale> getUserSaleList() { return userSaleList; }
+
+    public void setUserSaleList(List<UserSale> userSaleList) { this.userSaleList = userSaleList; }
+
+    public Account getAccount() { return account; }
+
+    public void setAccount(Account account) { this.account = account; }
 
     /**
      * View Sales List
@@ -82,6 +107,15 @@ public class UserSalesActionBean extends AbstractActionBean {
      */
     public Resolution insertAdoptForm(){
         return new ForwardResolution(INSERT_ADOPT);
+    }
+
+    /**
+     * Insert Adopt Form
+     *
+     * @return the resolution
+     */
+    public Resolution updateAdoptForm(){
+        return new ForwardResolution(UPDATE_ADOPT);
     }
 
     /**
